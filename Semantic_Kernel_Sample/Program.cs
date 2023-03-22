ab3ab2d3908624e9348bf7af7f600311ce73873c
francisco181288 <francisco.dominguez.porcel@gmail.com>
2023-03-22 20:50:54 +0100

    Agregar archivos de proyecto.

---
+++ Semantic_Kernel_Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Semantic_Kernel_Sample;
+using Microsoft.SemanticKernel;
+
+Console.WriteLine("Hola!!!");
+Console.WriteLine("cual es tu pregunta?");
+SemanticConnection gpt = new SemanticConnection();
+IKernel kernel = gpt.login();
+
+while (true)
+{
+    String texto;
+    texto = Console.ReadLine();
+    var answer = gpt.question(texto, kernel);
+    Console.WriteLine(answer);
+    kernel = gpt.login();
+}
---
+++ Semantic_Kernel_Sample/SemanticConnection.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.KernelExtensions;
+using Microsoft.SemanticKernel.Orchestration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semantic_Kernel_Sample
+{
+    internal class SemanticConnection
+    {
+        public IKernel login()
+        {
+
+            var kernel = Kernel.Builder.Build();
+
+            // For Azure Open AI service endpoint and keys please see
+            // https://learn.microsoft.com/azure/cognitive-services/openai/quickstart?pivots=rest-api
+            kernel.Config.AddOpenAICompletionBackend(
+                "GPT",  //nombre
+                "text-davinci-003",  // modelo
+                "sk-..." //APikey
+                );
+
+            return kernel;
+        }
+        public SKContext question(string text, IKernel kernel)
+        {
+
+            string skPrompt = text;
+
+            string textToSummarize = @"";
+
+            var tldrFunction = kernel.CreateSemanticFunction(skPrompt);
+
+            var summary = kernel.RunAsync(textToSummarize, tldrFunction).Result;
+
+            return summary;
+        }
+    }
+}
